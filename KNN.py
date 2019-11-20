76facae5d4ce7d4c34c6269c4066c6baf0145d3b
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2019-11-19 21:42:32 -0600

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -6,13 +6,13 @@ knn=KNeighborsClassifier(n_neighbors = 100)
 knn.fit(X_train_tf, y_train)
 
 y_train_preds = knn.predict_proba(X_train_tf)[:,1]
-y_valid_preds = knn.predict_proba(X_valid_tf)[:,1]
+y_test_preds = knn.predict_proba(X_test_tf)[:,1]
 
 print('KNN')
 print('Training:')
 knn_train_auc, knn_train_accuracy, knn_train_recall, knn_train_precision = print_report(y_train,y_train_preds, 0.5)
 print('Testing:')
-knn_testing_auc, knn_testing_accuracy, knn_testing_recall, knn_testing_precision = print_report(y_valid,y_valid_preds, 0.5)
+knn_testing_auc, knn_testing_accuracy, knn_testing_recall, knn_testing_precision = print_report(y_valid,y_test_preds, 0.5)
 
 from sklearn.metrics import roc_auc_score, accuracy_score, precision_score, recall_score
 def print_report(y_actual, y_pred, thresh):
