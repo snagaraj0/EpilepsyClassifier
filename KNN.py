94547db44715c9e99feac9c2b630a04e9fa974ac
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2019-11-19 21:05:31 -0600

    Create KNN.py

---
+++ KNN.py
@@ -0,0 +1,27 @@
+import warnings
+warnings.filterwarnings('ignore')
+from sklearn.neighbors import KNeighborsClassifier
+
+knn=KNeighborsClassifier(n_neighbors = 100)
+knn.fit(X_train_tf, y_train)
+
+y_train_preds = knn.predict_proba(X_train_tf)[:,1]
+y_valid_preds = knn.predict_proba(X_valid_tf)[:,1]
+
+print('KNN')
+print('Training:')
+knn_train_auc, knn_train_accuracy, knn_train_recall, knn_train_precision, knn_train_specificity = print_report(y_train,y_train_preds, thresh)
+print('Testing:')
+knn_valid_auc, knn_valid_accuracy, knn_valid_recall, knn_valid_precision, knn_valid_specificity = print_report(y_valid,y_valid_preds, thresh)
+
+from sklearn.metrics import roc_auc_score, accuracy_score, precision_score, recall_score
+def print_report(y_actual, y_pred, thresh):
+    auc = roc_auc_score(y_actual, y_pred)
+    accuracy = accuracy_score(y_actual, (y_pred > thresh))
+    recall = recall_score(y_actual, (y_pred > thresh))
+    precision = precision_score(y_actual, (y_pred > thresh))
+    print('AUC:%.3f'% auc)
+    print('accuracy:%.3f'%accuracy)
+    print('recall:%.3f'%recall)
+    print('precision:%.3f'%precision)
+    return auc, accuracy, recall, precision
