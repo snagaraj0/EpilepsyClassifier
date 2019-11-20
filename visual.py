32740bb3a78c53e25d09186df7f9d4c71e246cc6
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2019-11-19 22:17:05 -0600

    Update visual.py

--- visual.py
+++ visual.py
@@ -1,11 +1,11 @@
 import pandas as pd
-df_results = pd.DataFrame({'classifier':['KNN', 'KNN', 'Logistic','Logistic','SGD', 'SGD','ExtraTrees', 'ExtraTrees'],'data-set':['train', 'valid']*4,'auc':[knn_train_auc, knn_test_auc, lr_train_auc, lr_test_auc, sgd_train_auc, sgd_test_auc, etc_train_auc, etc_test_auc]})
+df_results = pd.DataFrame({'classifier':['KNN', 'KNN', 'Logistic','Logistic','SGD', 'SGD','ExtraTrees', 'ExtraTrees'],'dataset':['train', 'valid']*4,'auc':[knn_train_auc, knn_test_auc, lr_train_auc, lr_test_auc, sgd_train_auc, sgd_test_auc, etc_train_auc, etc_test_auc]})
 import seaborn as sb
 import matplotlib.pyplot as plt
 sb.set(style="whitegrid")
 plt.figure(figsize=(16, 8))
 #wahoo seaborn plot
-ax = sb.barplot(x = 'classifier', y = 'auc', hue = 'data_set', data = df_results)
+ax = sb.barplot(x = 'classifier', y = 'auc', hue = 'dataset', data = df_results)
 ax.set_xlabel('Classifier', fontsize = 20)
 ax.set_ylabel('AUC', fontsize = 20)
 ax.tick_params(labelsize = 20)
